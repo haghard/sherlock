97f1126c48129749cebd9ee4d79b461202fbfda4
Vadim Bondarev <haghard84@gmail.com>
2017-04-10 23:15:50 +0200

    add cluster membership

---
+++ src/main/scala/io/sherlock/ClusterMembership.scala
@@ -0,0 +1,45 @@
+package io.sherlock
+
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.cluster.{Cluster, Member, MemberStatus}
+import akka.cluster.ClusterEvent._
+
+import scala.collection.immutable.SortedSet
+
+object ClusterMembership {
+  def props(cluster: Cluster) = Props(new ClusterMembership(cluster))
+}
+
+class ClusterMembership(cluster: Cluster) extends Actor with ActorLogging {
+
+  override def preStart =
+    cluster.subscribe(self, classOf[ClusterDomainEvent])
+
+  private def evolve(clusterMembers: SortedSet[Member]): Receive = {
+    case MemberUp(member) =>
+      log.info("MemberUp = {}", member.address)
+      context become (evolve(clusterMembers + member))
+
+    case MemberExited(member) =>
+      log.info("MemberExited = {}", member.address)
+
+    case ReachableMember(member) =>
+      log.info("ReachableMember = {}", member.address)
+
+    case UnreachableMember(member) =>
+      log.info("UnreachableMember = {}", member.address)
+
+    case MemberRemoved(member, prev) =>
+      if (prev == MemberStatus.Exiting) log.info("{} gracefully exited", member.address)
+      else log.info("{} downed after being Unreachable", member.address)
+      context become evolve(clusterMembers - member)
+
+    case state: CurrentClusterState =>
+      log.info("Cluster state = {}", state.members)
+      context become evolve(state.members)
+
+    //case 'Members => sender() ! clusterMembers.mkString(",")
+  }
+
+  override def receive = evolve(SortedSet[Member]())
+}
