544dc22e13562a190d47466c121c231bf0306ae1
Vadim Bondarev <haghard84@gmail.com>
2017-06-27 23:36:37 +0200

    Added missing file

---
+++ src/main/scala/io/sherlock/OptsSupport.scala
@@ -0,0 +1,13 @@
+package io.sherlock
+
+trait OptsSupport {
+  val Opt = """(\S+)=(\S+)""".r
+
+  def argsToOpts(args: Seq[String]): Map[String, String] =
+    args.collect { case Opt(key, value) ⇒ key → value }.toMap
+
+  def applySystemProperties(options: Map[String, String]): Unit =
+    for ((key, value) ← options if key startsWith "-D") {
+      System.setProperty(key substring 2, value)
+    }
+}
